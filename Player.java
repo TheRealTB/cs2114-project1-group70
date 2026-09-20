c78681f9e491ed4d7a57b3e2393c4d9060049854
TheRealTB <phantomnotghost16@gmail.com>
2026-09-20 14:45:25 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,63 +1,85 @@
 package dungeonCrawler;
+
 public class Player {
-    //~ Fields ................................................................
+    // ~ Fields ................................................................
     private int maxHp;
     private int hp;
     private int atk;
-    private int def;
-    //etc...
-    
-    //~ Constructors ..........................................................
-    public Player(int maxHp, int atk, int def) {
-        this.maxHp=maxHp;
-        this.hp=maxHp;
-        this.atk=atk;
-        this.def=def;
+    private double def;
+    // etc...
+
+    // ~ Constructors ..........................................................
+    public Player(int maxHp, int atk, double def) {
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+        this.atk = atk;
+        this.def = def;
     }
-    //~Public  Methods ........................................................
-    
-    //getters
+    // ~Public Methods ........................................................
+
+
+    // getters
     public int getHp() {
         return hp;
     }
+
+
     public int getAtk() {
         return atk;
     }
-    public int getDef() {
+
+
+    public double getDef() {
         return def;
     }
-    //setters
+
+
+    // setters
     public int changeMaxHp(int change) {
-        if(maxHp+change<1) {
-            maxHp=1;
-        }else {
-            maxHp+=change;
+        if (maxHp + change < 1) {
+            maxHp = 1;
+        }
+        else {
+            maxHp += change;
         }
         return maxHp;
     }
+
+
     public int changeHp(int change) {
-        if(hp+change<0) {
-            hp=0;
-        }else if(hp+change>maxHp) {
-            hp=maxHp;
-        }else {
-            hp+=change;
+        if (hp + change < 0) {
+            hp = 0;
+        }
+        else if (hp + change > maxHp) {
+            hp = maxHp;
+        }
+        else {
+            hp += change;
         }
         return hp;
     }
+
+
     public int changeAtk(int change) {
-        if(atk+change<1) {
-            atk=1;
-        }else {
-            atk+=change;
+        if (atk + change < 1) {
+            atk = 1;
+        }
+        else {
+            atk += change;
         }
         return atk;
     }
-    public int changeDef(int change) {
-        if(def+change<1) {
-            def=1;
-        }else {
-            def+=change;
+
+
+    public double changeDef(double change) {
+        if (def + change < 0.01) {
+            def = 0.01;
+        }
+        else if (def + change > 0.99) {
+            def = 0.99;
+        }
+        else {
+            def += change;
         }
         return def;
     }
