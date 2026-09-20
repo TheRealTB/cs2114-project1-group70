ba7126b008671203dc34114665ecf00059a8d12f
TheRealTB <phantomnotghost16@gmail.com>
2026-09-20 16:20:14 -0400

    Update GameRunner.java

--- GameRunner.java
+++ GameRunner.java
@@ -11,10 +11,20 @@ import java.util.*;
  */
 public class GameRunner {
     //~ Fields ................................................................
-    private int stageNum;
-    
-    //~ Constructors ..........................................................
-
+    private static int depth;
     //~Public  Methods ........................................................
-
+    public static void startGame() {
+        Player player = new Player(100, 3, .9);
+        GameRunner.runTutorial();
+        GameRunner.runDungeonLoop();
+    }
+    public static void runTutorial() {
+        //tutorial
+    }
+    public static void runDungeonLoop() {
+        //make rooms and stuff
+    }
+    public static void endGame(int depthReached) {
+        
+    }
 }
