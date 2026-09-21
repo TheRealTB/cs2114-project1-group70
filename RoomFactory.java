aeee79ebb3c232fa438b03762c00f54d58d8be34
WillTheBestV <will.j.burnham@gmail.com>
2026-09-21 15:26:46 -0400

    Add RoomFactory for dungeon room creation
    Implemented RoomFactory class to create dungeon rooms based on depth.

--- RoomFactory.java
+++ RoomFactory.java
@@ -1,1 +1,35 @@
 
+package dungeonCrawler;
+ 
+// -------------------------------------------------------------------------
+/**
+ * Makes the rooms for the dungeon
+ *
+ * @author Will
+ * @version Sep 21, 2026
+ */
+public class RoomFactory {
+    // ~ Fields ................................................................
+    private static final double ENCOUNTER_CHANCE = 0.7;
+ 
+    // ----------------------------------------------------------
+    /**
+     * Makes a new room for the given depth.
+     *
+     * @param depth deepness
+     * @return EncounterRoom or UpgradeRoom
+     */
+    public static Room makeRoom(int depth) {
+        if (depth < 0) {
+            depth = 0;
+        }
+ 
+        if (depth > 0 && depth % 10 == 0) {
+            return EncounterRoom.generate(depth);
+        }
+         if (Math.random() < ENCOUNTER_CHANCE) {
+            return EncounterRoom.generate(depth);
+        }
+        return new UpgradeRoom(depth);
+    }
+}
