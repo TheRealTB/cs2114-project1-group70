60d26c32f7618c5e4039b021e5c7276687752be4
TheRealTB <phantomnotghost16@gmail.com>
2026-09-21 16:06:22 -0400

    Update UpgradeRoom.java

--- UpgradeRoom.java
+++ UpgradeRoom.java
@@ -6,6 +6,7 @@ public class UpgradeRoom extends Room {
     private double healUp;
     private double defUp;
     private double atkUp;
+
     // ~ Constructors ..........................................................
     public UpgradeRoom() {
         this(0);
@@ -17,32 +18,38 @@ public class UpgradeRoom extends Room {
         setDepth(depth);
         double rarity = Math.random();
         double healRoll = Math.random();
-        if(healRoll>.9) {
+        if (healRoll > .9) {
             healUp = 1.15;
-        }else if(healRoll>rarity) {
+        }
+        else if (healRoll > rarity) {
             healUp = 1.1;
-        }else {
+        }
+        else {
             healUp = 1.05;
         }
-        
+
         double atkRoll = Math.random();
-        if(atkRoll>.9) {
+        if (atkRoll > .9) {
             atkUp = 1.30;
-        }else if(atkRoll>rarity) {
+        }
+        else if (atkRoll > rarity) {
             atkUp = 1.20;
-        }else {
+        }
+        else {
             atkUp = 1.10;
         }
-        
+
         double defRoll = Math.random();
-        if(defRoll>.9) {
+        if (defRoll > .9) {
             defUp = .85;
-        }else if(defRoll>rarity) {
+        }
+        else if (defRoll > rarity) {
             defUp = .9;
-        }else {
+        }
+        else {
             defUp = .95;
         }
-        
+
     }
 
 
@@ -53,6 +60,26 @@ public class UpgradeRoom extends Room {
 
 
     public boolean enter(Player player, Scanner in) {
-        //plyaer enters and gets to choose between hp, def, or atk increase.
+        System.out.println("You have entered an Upgrade Room!");
+        System.out.println("Select a stat to improve:");
+        System.out.println("1. Max Health (x" + healUp + ")");
+        System.out.println("2. Attack (x" + atkUp + ")");
+        System.out.println("3. Defense (x" + defUp + ")");
+        String input = in.nextLine().trim().toLowerCase();
+        if (input.equals("Health")) {
+            // Replace with your actual player method, e.g.:
+            player.changeMaxHp((int)(player.getMaxHp() * healUp));
+            player.changeHp((int)(player.getHp() * healUp));
+            System.out.println("Your health has been increased!");
+        }
+        else if (input.equals("Attack")) {
+            player.changeAtk((int)(player.getAtk() * atkUp));
+            System.out.println("Your attack has been increased!");
+        }
+        else if (input.equals("Defense")) {
+            player.changeDef((int)(player.getDef() * defUp));
+            System.out.println("Your defense has been increased!");
+        }
+        return true;
     }
 }
